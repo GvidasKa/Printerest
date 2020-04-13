0cb5c58d8e05a79e4efc794651f798df1803f1e0
GvidasKa <gvidas@scoding.com>
2020-04-13 22:12:08 +0300

    Setting up vue to be displayed on home page , creating pins and comments tables , models and creating relationships

---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-info mb-2">
+        <div class="container">
+            <a href="#" class="navbar-brand"><img src="https://www.stickpng.com/assets/images/580b57fcd9996e24bc43c52e.png" style="width:50px;"></a>
+        </div>
+    </nav>
+</template>
